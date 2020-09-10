f51b4e77f94e96f08c5e70af7445b148d80a86f6
allan <allan.nava@ies-italia.it>
2020-09-10 18:54:49 +0200

    fix src with new layout

--- src/components/organisms/slideContent.js
+++ src/components/organisms/slideContent.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-
 export default function SlideContent(props) {
-    let Background = process.env.PUBLIC_URL + props.path
     return (
         <div className="container">
-            <div className="slide-content-text">
-                <h1 className="slide-title">{props.title}</h1>
-                <p className="slide-paragraph">Cras ac mi diam. Donec condimentum eget
-                egestas. Nam diam eros, convallis quis justo in,
-                pellentesque arcu convallis quis justo in, dictum.
-Aenean vehicula accumsan rhoncus. as </p>
-            </div>
-            <div className="slide-content-image" style={{ backgroundImage: `url(${Background})` }}>
-
+            <h1 className="slide-content-title">{props.title}</h1>
+            <p className="slide-content-text">Cras ac mi diam. Donec condimentum eget
+            egestas. Nam diam eros, convallis quis justo in,
+            pellentesque arcu convallis quis justo in, dictum.
+Aenean vehicula accumsan rhoncus. </p>
+            <div className="slide-content-image">
+                <p>IMAGE HERE</p>
             </div>
         </div>
     )
